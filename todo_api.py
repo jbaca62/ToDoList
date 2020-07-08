5cfb1f63beb946fa24f4cd9acb4b230c3423b6dc
Jacob Baca <43391464+jbaca62@users.noreply.github.com>
2020-07-08 14:07:36 -0700

    test

--- todo_api.py
+++ todo_api.py
@@ -12,7 +12,11 @@ class ToDoAPI:
         return tasks
 
     def add_task(task_title, is_child=False, parent_id=0):
+<<<<<<< Updated upstream
         url = 'http://127.0.0.1:5000/add'
+=======
+        url = 'http://192.168.43.33:5000/add'
+>>>>>>> Stashed changes
         task = {'task_title': task_title,
                 'is_child': is_child,
                 'parent_id': parent_id}
@@ -21,7 +25,11 @@ class ToDoAPI:
         return response.text
 
     def complete_task(task_id):
+<<<<<<< Updated upstream
         url = 'http://127.0.0.1:5000/complete'
+=======
+        url = 'http://192.168.43.33:5000/complete'
+>>>>>>> Stashed changes
         task = {'task_id': task_id}
         response = requests.post(url, json = task)
 
