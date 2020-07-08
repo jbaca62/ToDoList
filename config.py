c27f4acf6efbaec96e2fc2afb34e9a2d4ef3276c
Jacob Baca <43391464+jbaca62@users.noreply.github.com>
2020-07-08 16:20:36 -0700

    add config

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
+<<<<<<< Updated upstream
 db_config = {
                 "host": "localhost",
+=======
+DB_CONFIG = {
+                "host": "192.168.43.33",
+>>>>>>> Stashed changes
                 "port": "3306",
                 "user": "todolist",
                 "password": "todolist",
                 "database": "todolist",
+<<<<<<< Updated upstream
                 }
+=======
+                }
+
+API_URL = 'http://127.0.0.1:5000'
+>>>>>>> Stashed changes
--- task_list.py
+++ task_list.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 import mysql.connector
+<<<<<<< Updated upstream
 from config import db_config
+=======
+import config
+>>>>>>> Stashed changes
 
 insert_task = "INSERT INTO tasks (title, completed, creation_date, is_child_task, parent_id) VALUES (%s, %s, %s, %s, %s)"
 get_incompleted_tasks = "SELECT * FROM tasks WHERE completed = 0"
@@ -8,13 +12,6 @@ get_all_tasks = "SELECT * FROM tasks"
 set_task_to_complete_by_id = "UPDATE tasks SET completed = 1 WHERE id = %s"
 
 class Task:
-    db_config = {
-                "host": "localhost",
-                "port": "3306",
-                "user": "todolist",
-                "password": "todolist",
-                "database": "todolist",
-                }
     def __init__(self, task_id, title, completed=False, creation_date=datetime.now()):
         self.id = task_id
         self.title = title
@@ -29,11 +26,11 @@ class Task:
                     is_child=False, parent_id=0):
         vals = (title, completed, creation_date, is_child, parent_id)
         mydb = mysql.connector.connect(
-            host=Task.db_config["host"],
-            port=Task.db_config["port"],
-            user=Task.db_config["user"],
-            password=Task.db_config["password"],
-            database=Task.db_config["database"]
+            host=config.DB_CONFIG["host"],
+            port=config.DB_CONFIG["port"],
+            user=config.DB_CONFIG["user"],
+            password=config.DB_CONFIG["password"],
+            database=config.DB_CONFIG["database"]
         )
         mycursor = mydb.cursor()
         mycursor.execute(insert_task, vals)
@@ -48,11 +45,11 @@ class Task:
 
     def get_incompleted_tasks():
         mydb = mysql.connector.connect(
-            host=Task.db_config["host"],
-            port=Task.db_config["port"],
-            user=Task.db_config["user"],
-            password=Task.db_config["password"],
-            database=Task.db_config["database"]
+            host=config.DB_CONFIG["host"],
+            port=config.DB_CONFIG["port"],
+            user=config.DB_CONFIG["user"],
+            password=config.DB_CONFIG["password"],
+            database=config.DB_CONFIG["database"]
         )
         mycursor = mydb.cursor()
         mycursor.execute(get_incompleted_tasks)
@@ -94,11 +91,11 @@ class Task:
             task_id = str(task_id)
         
         mydb = mysql.connector.connect(
-            host=Task.db_config["host"],
-            port=Task.db_config["port"],
-            user=Task.db_config["user"],
-            password=Task.db_config["password"],
-            database=Task.db_config["database"]
+            host=config.DB_CONFIG["host"],
+            port=config.DB_CONFIG["port"],
+            user=config.DB_CONFIG["user"],
+            password=config.DB_CONFIG["password"],
+            database=config.DB_CONFIG["database"]
         )
         mycursor = mydb.cursor()
         task_id = (task_id,)
@@ -117,11 +114,11 @@ class Task:
 
     def get_database_tasks():
         mydb = mysql.connector.connect(
-            host=Task.db_config["host"],
-            port=Task.db_config["port"],
-            user=Task.db_config["user"],
-            password=Task.db_config["password"],
-            database=Task.db_config["database"]
+            host=config.DB_CONFIG["host"],
+            port=config.DB_CONFIG["port"],
+            user=config.DB_CONFIG["user"],
+            password=config.DB_CONFIG["password"],
+            database=config.DB_CONFIG["database"]
         )
         mycursor = mydb.cursor()
         mycursor.execute(get_all_tasks)
