beab3f6b0345510c7e082e0476ea4e7eee5ad5dd
Jacob Baca <43391464+jbaca62@users.noreply.github.com>
2020-07-09 14:08:46 -0700

    started angular frontend

---
+++ to-do-list/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { HttpClientModule }    from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
