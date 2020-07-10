7fd366495928ee5a85dd8d675ba4e768aff54a01
Jacob Baca <43391464+jbaca62@users.noreply.github.com>
2020-07-10 16:59:01 -0700

    remade angular project

--- to-do-list-old/src/app/app.module.ts
+++ to-do-list-old/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { HttpClientModule }    from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
