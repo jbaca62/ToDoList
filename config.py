04935090213d95561cc03085c723337b18b97b85
Jacob Baca <43391464+jbaca62@users.noreply.github.com>
2020-07-14 14:38:31 -0700

    updated config for server use

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 DB_CONFIG = {
-                "host": "192.168.43.33",
+                "host": "127.0.0.1",
                 "port": "3306",
                 "user": "todolist",
                 "password": "todolist",
                 "database": "todolist",
                 }
 
-API_URL = 'http://127.0.0.1:5000'
+API_URL = 'http://192.168.43.33:5000'
